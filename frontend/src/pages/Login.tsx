c70f7b1d6e70c759cdcf2593f5e26a566923328c
swarupkkk <swarupkkashid.tekhnologia@gmail.com>
2024-10-16 18:35:37 +0530

    2 commit message

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 import { loginUser } from '../services/api';
 
@@ -29,6 +30,7 @@ const Login = () => {
 
   return (
     <div className="min-h-screen flex items-center justify-center">
+      <div>
       <form onSubmit={handleLogin} className="bg-white p-6 rounded shadow-md w-full max-w-md">
         <h1 className="text-2xl mb-6">Login</h1>
         {error && <p className="text-red-600">{error}</p>}
@@ -50,6 +52,12 @@ const Login = () => {
         />
         <button className="w-full bg-blue-600 text-white py-2">Login</button>
       </form>
+      <p className='text-semibold text-2xl text-center bg-red-200'>Don't Have an account?<Link to='/signup'><button
+            className=" text-black font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          >
+            Signup
+          </button></Link> </p>
+      </div>
     </div>
   );
 };
